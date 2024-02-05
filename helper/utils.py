abf04f57725e68634dac399d2a0a1a8df51af470
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-02-05 06:35:22 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -70,17 +70,6 @@ def convert(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
-async def send_log(b, u):
-    if Config.LOG_CHANNEL is not None:
-        curr = datetime.now(timezone("Asia/Kolkata"))
-        date = curr.strftime('%d %B, %Y')
-        time = curr.strftime('%I:%M:%S %p')
-        await b.send_message(
-            Config.LOG_CHANNEL,
-            f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: @{Config.BOT_USERNAME}"
-        )
-
-
 def humanbytes(size):
     """Convert Bytes To Bytes So That Human Can Read It"""
     if not size:
