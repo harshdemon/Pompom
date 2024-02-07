9a86362ae06cbccf0eaa2b7e338bee719a6d142e
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-02-07 14:32:51 +0530

    Update download_link.py

--- plugins/download_link.py
+++ plugins/download_link.py
@@ -17,71 +17,73 @@ from helper.utils import (
     progress_for_pyrogram,
 )
 
-
 class Downloader:
     def __init__(self):
         self.queue_links = {}
 
-    async def download_multiple(self, bot, update, link_msg):
+    async def download_multiple(self, bot, update, link_msg, index=0):
         user_id = update.from_user.id
+        msg = await update.message.reply_text(
+            f"**{index+1}. Link:-** {self.queue_links[user_id][index]}\n\nDownloading... Please Have Patience\n 𝙇𝙤𝙖𝙙𝙞𝙣𝙜...",
+            disable_web_page_preview=True
+        )
 
-        for index, link in enumerate(self.queue_links[user_id]):
+        # Set options for youtube-dl
+        if str(self.queue_links[user_id][index]).startswith("https://www.pornhub"):
+            thumbnail = get_porn_thumbnail_url(self.queue_links[user_id][index])
+        else:
+            thumbnail = get_thumbnail_url(self.queue_links[user_id][index])
 
-            msg = await update.message.reply_text(
-                f"**{index+1}. Link:-** {link}\n\nDownloading... Please Have Patience\n 𝙇𝙤𝙖𝙙𝙞𝙣𝙜...",
-                disable_web_page_preview = True
-            )
+        ytdl_opts = {
+            'format': 'best',
+            'progress_hooks': [lambda d: download_progress_hook(d, msg, self.queue_links[user_id][index])],
+        }
+        # loop = asyncio.get_event_loop()
 
-            # Set options for youtube-dl
-            if str(self.queue_links[user_id][index]).startswith("https://www.pornhub"):
-                thumbnail = get_porn_thumbnail_url(
-                    self.queue_links[user_id][index])
-            else:
-                thumbnail = get_thumbnail_url(self.queue_links[user_id][index])
+        with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+            try:
+                # await loop.run_in_executor(None, ydl.download, [self.queue_links[user_id][index]])
+                ydl.download([self.queue_links[user_id][index]])
+            except DownloadError:
+                await msg.edit("Sorry, There was a problem with that particular video")
+                return
 
-            ytdl_opts = {
-                'format': 'best',
-                'progress_hooks': [lambda d: download_progress_hook(d, msg, self.queue_links[user_id][index])],
-            }
-            # loop = asyncio.get_event_loop()
+        # Generate a unique filename for the thumbnail
+        unique_id = uuid.uuid4().hex
+        if thumbnail:
+            thumbnail_filename = f"p_hub_thumbnail_{unique_id}.jpg"
 
-            with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+            # Download the thumbnail image
+            response = requests.get(thumbnail)
+            if response.status_code == 200:
+                with open(thumbnail_filename, 'wb') as f:
+                    f.write(response.content)
+
+        await msg.edit("⚠️ Please Wait...\n\n**Trying to Upload....**")
+
+        for file in os.listdir('.'):
+            if file.endswith(".mp4") or file.endswith('.mkv'):
                 try:
-                    # await loop.run_in_executor(None, ydl.download, [self.queue_links[user_id][index]])
-                    ydl.download([self.queue_links[user_id][index]])
-                except DownloadError:
-                    await msg.edit("Sorry, There was a problem with that particular video")
-                    return
+                    await self.send_video(bot, update, file, thumbnail_filename, msg)
+                    break
+                except Exception as e:
+                    print("⚠️  ERROR:- ", e)
+                    break
+            else:
+                continue
 
-            # Generate a unique filename for the thumbnail
-            unique_id = uuid.uuid4().hex
-            if thumbnail:
-                thumbnail_filename = f"p_hub_thumbnail_{unique_id}.jpg"
+        await msg.delete()
 
-                # Download the thumbnail image
-                response = requests.get(thumbnail)
-                if response.status_code == 200:
-                    with open(thumbnail_filename, 'wb') as f:
-                        f.write(response.content)
-
-            await msg.edit("⚠️ Please Wait...\n\n**Trying to Upload....**")
-
-            for file in os.listdir('.'):
-                if file.endswith(".mp4") or file.endswith('.mkv'):
-                    try:
-                        await self.send_video(bot, update, file, thumbnail_filename, msg)
-                        break
-                    except Exception as e:
-                        await msg.edit("⚠️  ERROR:- ", e)
-                        break
-                else:
-                    continue
-
-            await msg.delete()
-        try:
-            return await update.message.reply_text(f"ALL LINKS DOWNLOADED SUCCESSFULLY ✅",  reply_to_message_id=link_msg.id)
-        except:
-            await update.message.reply_text(f"ALL LINKS DOWNLOADED SUCCESSFULLY ✅")
+        if self.queue_links[user_id][index] == self.queue_links[user_id][-1]:
+            self.queue_links.pop(user_id)
+            index = 0
+            try:
+                return await update.message.reply_text(f"ALL LINKS DOWNLOADED SUCCESSFULLY ✅",  reply_to_message_id=link_msg.id)
+            except:
+                await update.message.reply_text(f"ALL LINKS DOWNLOADED SUCCESSFULLY ✅")
+        else:
+            index += 1
+            await self.download_multiple(bot, update, link_msg, index)
 
     async def send_video(self, bot, update, file, thumbnail_filename, msg):
         user_id = update.from_user.id
@@ -92,8 +94,7 @@ class Downloader:
                 thumb=thumbnail_filename,
                 caption=f"**📁 File Name:- `{file}`\n\nHere Is your Requested Video 🔥**\n\nPowered By - @{Config.BOT_USERNAME}",
                 progress=progress_for_pyrogram,
-                progress_args=(
-                    "\n⚠️ Please Wait...\n\n**Uploading Started...**", msg, time.time())
+                progress_args=("\n⚠️ Please Wait...\n\n**Uploading Started...**", msg, time.time())
             )
             os.remove(thumbnail_filename)
         else:
@@ -102,25 +103,20 @@ class Downloader:
                 video=file,
                 caption=f"**📁 File Name:- `{file}`\n\nHere Is your Requested Video 🔥**\n\nPowered By - @{Config.BOT_USERNAME}",
                 progress=progress_for_pyrogram,
-                progress_args=(
-                    "\n⚠️ Please Wait...\n\n**Uploading Started...**", msg, time.time())
+                progress_args=("\n⚠️ Please Wait...\n\n**Uploading Started...**", msg, time.time())
             )
         os.remove(file)
 
-
 downloader = Downloader()
-
 
 @Client.on_message(filters.regex(pattern=r"(?=.*https://)(?!.*\bmega\b).*"))
 async def handle_yt_dl(bot: Client, cmd: Message):
     await cmd.reply_text("**Do you want to download this file ?**", reply_to_message_id=cmd.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔻 Download 🔻', callback_data='http_link')], [InlineKeyboardButton('🖇️ Add Multiple Links 🖇️', callback_data='multiple_http_link')]]))
 
-
 @Client.on_callback_query(filters.regex('^http_link'))
 async def handle_single_download(bot: Client, update: CallbackQuery):
     http_link = update.message.reply_to_message.text
     await ytdl_downloads(bot, update, http_link)
-
 
 @Client.on_callback_query(filters.regex('^multiple_http_link'))
 async def handle_multiple_download(bot: Client, update: CallbackQuery):
@@ -147,10 +143,6 @@ async def handle_multiple_download(bot: Client, update: CallbackQuery):
                         links += f"{(idx+1)}. `{link}`\n"
 
                     links_msg = await update.message.reply_text(f"👤 <code>{update.from_user.first_name}</code>\n\n {links}")
-                    break
-                elif link.text == "/clear":
-                    downloader.queue_links.pop(user_id)
-                    await update.message.reply_text("Successfully Cleared ✅")
                     break
                 else:
                     await update.answer("⚠️ Please Send Valid Link !")
